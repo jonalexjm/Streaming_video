19e5344e3d8850c8c2f12e623ff9837d4fbf4a2e
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2020-05-08 11:22:07 -0500

    show imagen in browser

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var app = require('express')(),
+    http = require('http').createServer(app),
+    io = require('socket.io')(http),
+    port = process.env.PORT || 3000,
+    publicDir = `${__dirname}/public`;
+
+
+http.listen(port, ()=>{
+    console.log('Iniciando Express y Socket.IO en Localhost:%d', port);
+
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile(`${publicDir}/client.html`)
+})
+   .get('/streaming', (req, res)=>{
+    res.sendFile(`${publicDir}/server.html`)
+   });
+
+io.on('connection', (socket) => {
+
+    socket.on('stream', function(image){
+
+        socket.broadcast.emit('stream', image);
+    });
+})
